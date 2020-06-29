ca644d93018e83c0d4d4c6c241a40c67448b5811
LuannaCorreia <lusinha995@gmail.com>
2020-06-29 14:57:51 -0300

    Aula 25 finalizada

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             Fibonacci rec = new Fibonacci();
+            rec.GerarSequeunciaDeFibonacci(0,3,37);
+            System.Console.WriteLine(rec.GerarFatorial(23));
+        }
+    }
+}
